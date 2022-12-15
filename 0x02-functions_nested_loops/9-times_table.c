dbca1d20517f37115d2d541c4d649fd540f6f1ac
ivutha2 <ireneivutha1@gmail.com>
2022-12-14 16:13:22 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,11 +6,11 @@
 void times_table(void)
 {
 	int num, mult, prod;
-	
+
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar('0');
-		
+		_putchar(0);
+
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
@@ -21,9 +21,7 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) * '0');
-			_putchar((prod % 10) * '0');
+				_putchar((prod / 10) + '0');
 		}
-		_putchar('\n');
 	}
 }
