d8b5ad5b46e24dd270bff3304db8e035fc606cf6
ivutha2 <ireneivutha1@gmail.com>
2022-12-22 10:34:08 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,9 +8,9 @@
 char *leet(char *str)
 {
 	int index1 = 0, index2;
-	char leet[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (str[++index1])
+	while (str[index1])
 	{
 		for (index2 = 0; index2 <= 7; index2++)
 		{
@@ -18,6 +18,8 @@ char *leet(char *str)
 					str[index1] - 32 == leet[index2])
 				str[index1] = index2 + '0';
 		}
+
+		index1++;
 	}
 	return (str);
 }
